40f9d58d65d67b516361737ab3ac5e07d540ab4b
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-04-02 16:40:26 -0400

    import utils from `mdq`

--- pxul/os.py
+++ pxul/os.py
@@ -9,6 +9,9 @@ CHANGES:
      - Add `StackDir`
      - Add `ensure_dir`
      - Add `ensure_file`
+     - Add `find_in_path`
+     - Add `find_in_root`
+     - Add `SetEnv`
 """
 
 import os
@@ -44,6 +47,47 @@ class StackDir(object):
         """
         self.__exit__()
 
+class SetEnv(object):
+    """
+    Set the environment variables in `os.environ`.
+
+    >>> with SetEnv(PATH='/foo/bar/bin', SPAM='eggs'):
+    ...   print os.environ['PATH']
+    ...   print os.environ['SPAM']
+    /foo/bar/bin
+    eggs
+    """
+    def __init__(self, **env):
+        self._new_env = env
+        self._old_env = dict()
+
+    def __enter__(self):
+        for name, value in self._new_env.iteritems():
+            value = str(value)
+            if name in os.environ:
+                self._old_env[name] = os.environ[name]
+            os.environ[name] = value
+        return self
+
+    def __exit__(self, typ, value, traceback):
+        for name, value in self._new_env.iteritems():
+            if name in self._old_env:
+                os.environ[name] = self._old_env[name]
+            else:
+                del os.environ[name]
+
+    def set(self):
+        """
+        Set the environment
+        """
+        self.__enter__()
+
+    def unset(self):
+        """
+        Undo the changes to the environment
+        """
+        self.__exit__()
+
 def ensure_dir(path):
     """
     Make sure the `path` if a directory by creating it if needed.
@@ -59,3 +103,39 @@ def ensure_file(path):
     root = os.path.dirname(path)
     ensure_dir(root)
     open(path, 'w').close()
+
+
+def find_in_path(exe, search=None):
+    """
+    Attempts to locate the given executable in the provided search paths.
+    If `search` is none the PATH environment variable is searched.
+
+    Params:
+     exe :: <str> = executable name
+     search :: <list of str> = search paths
+
+    Returns:
+     Either None or full path :: <str>
+    """
+
+    search = search if search is not None else os.environ['PATH'].split(os.pathsep)
+    for prefix in search:
+        path = os.path.join(prefix, exe)
+        if os.path.exists(path) and os.access(path, os.X_OK):
+            return path
+
+def find_in_root(exe, root='/'):
+    """
+    Attempts to find the executable name by traversing the directory structure starting at `root`
+
+    Params:
+     exe :: <str> = executable name
+     root :: <str> = prefix to search under
+
+    Returns:
+      Either None or path :: <str>
+    """
+    for dirpath, dirnames, filenames in os.walk(root):
+        path = os.path.join(dirpath, exe)
+        if exe in filenames and os.access(path, os.X_OK):
+            return path
