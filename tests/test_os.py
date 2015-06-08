26c0fd97d6d59a0a4254c11d0585f4df75aae1d9
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2015-06-07 22:49:41 -0400

    initial tests

---
+++ tests/test_os.py
@@ -0,0 +1,171 @@
+import pxul.os
+import os
+import os.path
+import tempfile
+import shutil
+import uuid
+
+from unittest import TestCase, expectedFailure
+
+
+class TmpDir_Test(TestCase):
+    def test_directory_changed(self):
+        "Entering the context should temporarily switch to a different working directory"
+
+        starting_cwd = os.getcwd()
+        with pxul.os.TmpDir():
+            new_cwd = os.getcwd()
+            self.assertNotEqual(starting_cwd, new_cwd)            
+
+    def test_cleanup(self):
+        "Exiting the context should remove the temporary directory"
+
+        starting_cwd = os.getcwd()
+        with pxul.os.TmpDir():
+            new_cwd = os.getcwd()
+            self.assertTrue(os.path.exists(new_cwd))
+        final_cwd = os.getcwd()
+        self.assertEqual(starting_cwd, final_cwd)
+        self.assertFalse(os.path.exists(new_cwd))
+
+    @expectedFailure
+    def test_context_as_keyword(self):
+        "Entering should return the new path when using `with ctx as name`"
+
+        with pxul.os.TmpDir() as tmpdir:
+            cwd = os.getcwd()
+            self.assertIsNotNone(tmpdir)
+            self.assertEqual(tmpdir, cwd)
+
+
+class StackDir_Test(TestCase):
+    def test_directory_changed(self):
+        "Entering the context should temporarily change directories"
+        newdir = os.getenv('HOME')
+        starting_cwd = os.getcwd()
+        with pxul.os.StackDir(newdir):
+            new_cwd = os.getcwd()
+            self.assertNotEqual(starting_cwd, new_cwd)
+            self.assertEqual(newdir, new_cwd)
+
+    def test_cleanup(self):
+        "Exiting the context should not remove the target directory"
+
+        tmpdir = tempfile.mkdtemp()
+        try:
+            with pxul.os.StackDir(tmpdir):
+                self.assertTrue(os.path.exists(tmpdir))
+            self.assertTrue(os.path.exists(tmpdir))
+        finally:
+            os.rmdir(tmpdir)
+
+
+class SetEnv_Test(TestCase):
+    def new_env(self):
+        name = 'SET_ENV_TEST_{}'.format(uuid.uuid4().hex)
+        value = '{}'.format(uuid.uuid4().hex)
+        return name, value
+
+    def test_env_set(self):
+        "Entering the context with keywords should update the environment"
+        k0, v0 = self.new_env()
+        self.assertIsNone(os.getenv(k0))
+
+        k1, v1 = self.new_env()
+        self.assertIsNone(os.getenv(k1))
+
+        with pxul.os.SetEnv(**{k0: v0, k1: v1}):
+            self.assertEqual(v0, os.getenv(k0))
+            self.assertEqual(v1, os.getenv(k1))
+
+    def test_cleanup(self):
+        "Exiting the context should clear the environment"
+
+        k, v = self.new_env()
+        with pxul.os.SetEnv(**{k: v}):
+            pass
+        self.assertIsNone(os.getenv(k))
+
+
+class clear_dir_Test(TestCase):
+    def test_cleanup(self):
+        tmpdir = tempfile.mkdtemp()
+        hello = os.path.join(tmpdir, 'hello.txt')
+        try:
+            with open(hello, 'w') as fd:
+                fd.write('world\n')
+
+            # should not be empty
+            with self.assertRaises(OSError):
+                os.rmdir(tmpdir)
+
+            pxul.os.clear_dir(tmpdir)
+
+            # show now be empty
+            os.rmdir(tmpdir)
+            
+        finally:
+            if os.path.exists(tmpdir):
+                shutil.rmtree(tmpdir)
+
+
+class ensure_dir_Test(TestCase):
+    def test_absent(self):
+        "ensure_dir should create a directory if not present"
+        tmpname = 'tmp{}'.format(uuid.uuid4().hex)
+        tmpdir = os.path.join(tempfile.tempdir, tmpname)
+
+        try:
+            self.assertFalse(os.path.exists(tmpdir))
+            pxul.os.ensure_dir(tmpdir)
+            self.assertTrue(os.path.exists(tmpdir))
+        finally:
+            if os.path.exists(tmpdir):
+                os.rmdir(tmpdir)
+
+    def test_present(self):
+        "ensure_dir should not modify a directory that is already present"
+
+        with pxul.os.TmpDir():
+            cwd = os.getcwd()
+            self.assertTrue(os.path.exists(cwd))
+
+            stat_before = os.stat(cwd)
+            pxul.os.ensure_dir(cwd)
+            stat_after = os.stat(cwd)
+
+            self.assertTrue(os.path.exists(cwd))
+            self.assertEqual(stat_before, stat_after)
+
+class fullpath_Test(TestCase):
+    def test_expand_user(self):
+        "Should expand ~"
+        home = os.getenv('HOME')
+        expanded = pxul.os.fullpath('~')
+        self.assertEqual(home, expanded)
+
+    def test_expand_vars(self):
+        "Should expand variables"
+        home = os.getenv('HOME')
+        expanded = pxul.os.fullpath('$HOME')
+        self.assertEqual(home, expanded)
+
+    def test_abspath(self):
+        "Should find the absolute path"
+        name = uuid.uuid4().hex
+        cwd = os.getcwd()
+        real = os.path.join(cwd, name)
+        expanded = pxul.os.fullpath(name)
+        self.assertEqual(real, expanded)
+
+
+class ensure_file_Test(TestCase):
+    def test_absent(self):
+        "Should create an empty file if it does not exist"
+
+        with pxul.os.TmpDir():
+            name = 'hello.txt'
+
+            self.assertFalse(os.path.exists(name))
+            pxul.os.ensure_file(name)
+            self.assertTrue(os.path.exists(name))
