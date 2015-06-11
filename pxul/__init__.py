1448ce10eec1749c53d7843886bd808145e9280e
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2015-06-11 12:34:31 -0400

    handle missing version module

--- pxul/__init__.py
+++ pxul/__init__.py
@@ -1,1 +1,5 @@
-from .version import version, time_version, git_version, full_version
+
+try:
+    from .version import version, time_version, git_version, full_version
+except ImportError:
+    version = 'UNKNOWN'
